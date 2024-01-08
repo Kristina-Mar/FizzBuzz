b37cbb5e61314b8efd0cfabd3759519a3031b23c
Kristina Martiskova <kris.martiskova@gmail.com>
2024-01-08 21:01:38 +0100

    Add project files.

---
+++ CheckNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class CheckNumber
+    {
+        public static bool IsDivisibleBy3(int number)
+        {
+            return number % 3 == 0;
+        }
+        public static bool IsDivisibleBy5(int number)
+        {
+            return number % 5 == 0;
+        }
+        public static bool IsDivisibleBy3And5(int number)
+        {
+            return IsDivisibleBy3(number) && IsDivisibleBy5(number);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 1; i < 101; i++)
+            {
+                if (CheckNumber.IsDivisibleBy3And5(i))
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (CheckNumber.IsDivisibleBy3(i))
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (CheckNumber.IsDivisibleBy5(i))
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
